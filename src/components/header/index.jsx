e0bee90b4736395fdab8297d873fbe9ce0f178e2
1440407551 <1440407551@qq.com>
2019-09-19 20:34:22 +0800

    update

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -6,7 +6,18 @@ export default class Header extends React.Component {
     render() {
         return (
             <div className="header">
-                Header
+                <div className="header-top">
+                    欢迎，admin
+                    <a href="javascript:">退出</a>
+                </div>
+                <div className="header-bottom">
+                    <div className="header-bottom-left">角色管理</div>
+                    <div className="header-bottom-right">
+                        <span>2019-09-19 12:12:12</span>
+                        <img src="http://api.map.baidu.com/images/weather/day/duoyun.png" alt="weather"/>
+                        <span>多云</span>
+                    </div>
+                </div>
             </div>
         )
     }
