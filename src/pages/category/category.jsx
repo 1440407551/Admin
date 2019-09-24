e2e1365f252cfc3a29d2672863fa857f3c4369ba
1440407551 <1440407551@qq.com>
2019-09-24 20:04:36 +0800

    update

--- src/pages/category/category.jsx
+++ src/pages/category/category.jsx
@@ -3,12 +3,14 @@ import {
   Card,
   Button,
   Icon,
-  Table
+  Table,
+  message,
+  Modal
 } from 'antd'
 
-import { reqCategorys } from '../../api'
+import AddUpdateForm from './add-update-form'
+import { reqCategorys, reqAddCategory, reqUpdateCategory } from '../../api'
 import LinkButton from '../../components/link-button'
-import { async } from 'q'
 
 /**
  * 分类管理
@@ -17,14 +19,8 @@ export default class Category extends Component {
 
   state = {
     categorys: [], // 所有分类的数组
-  }
-
-  UNSAFE_componentWillMount() {
-    this.initColumns()
-  }
-
-  componentDidMount() {
-    this.getCategorys()
+    loading: false, // 是否正在请求加载中
+    showStatus: 0, //0: 不显示, 1: 显示添加, 2: 显示修改 
   }
 
   /**
@@ -39,7 +35,10 @@ export default class Category extends Component {
       {
         title: '操作',
         width: 300,
-        render: () => <LinkButton>修改分类</LinkButton>
+        render: (category) => <LinkButton onClick={() => {
+          this.category = category // 保存当前分类，其他地方都可以读取到
+          this.setState({ showStatus: 2 })
+        }}>修改分类</LinkButton>
       },
     ];
   }
@@ -48,17 +47,90 @@ export default class Category extends Component {
    * 异步获取分类列表
    */
   getCategorys = async () => {
-    await reqCategorys()
+    // 显示loading
+    this.setState({ loading: true })
+    // 发异步ajax请求
+    const result = await reqCategorys()
+    this.setState({ loading: false })
+    if (result.status === 0) { // 成功了
+      // 更新状态categorys数据
+      const categorys = result.data
+      this.setState({
+        categorys
+      })
+    } else {
+      message.error('获取分类列表失败')
+    }
+
   }
 
+  /**
+   * 点击确定的回调: 去添加/修改分类
+   */
+  handleOk = () => {
+
+    // 进行表单验证
+    this.form.validateFields(async (err, values) => {
+      if (!err) {
+        
+        // 验证通过后，得到输入数据
+        const { categoryName } = values
+
+        const { showStatus } = this.state
+
+        let result
+        if (showStatus === 1) { // 添加
+          // 发添加分类的请求
+          result = await reqAddCategory(categoryName)
+        } else { // 修改
+          const categoryId = this.category._id
+          result = await reqUpdateCategory({ categoryId, categoryName })
+        }
+
+        const action = showStatus === 1 ? '添加' : '修改'
+        // 根据响应结果，做不同处理
+        this.setState({ showStatus: 0 })
+        
+        if (result.status === 0) {
+          //  重新获取分类列表显示 
+          this.getCategorys()
+          message.success(action + '分类成功')
+        } else {
+          message.error(action + '分类失败')
+        }
+        this.form.resetFields() // 重置输入数据(变成了初始值)
+      }
+    })
+  }
+
+  /**
+   * 点击取消的回调
+   */
+  handleCancel = () => {
+    this.form.resetFields() // 重置输入数据(变成了初
+    this.setState({
+      showStatus: 0
+    })
+  }
+
+  UNSAFE_componentWillMount() {
+    this.initColumns()
+  }
+
+  componentDidMount() {
+    this.getCategorys()
+  }
 
   render() {
     // 取出状态数据
-    const { categorys } = this.state
+    const { categorys, loading, showStatus } = this.state
+
+    // 读取更新的分类名称
+    const category = this.category || {}
 
     // Card右上角的结构
     const extra = (
-      <Button type="primary">
+      <Button type="primary" onClick={() => this.setState({ showStatus: 1 })}>
         <Icon type="plus" />
         添加
       </Button>
@@ -70,10 +142,21 @@ export default class Category extends Component {
         <Table
           bordered
           rowKey="_id"
+          loading={loading}
           columns={this.columns}
           dataSource={categorys}
-          pagination={{ defaultPageSize: 2 }}
+          pagination={{ defaultPageSize: 6 }}
         />
+
+        <Modal
+          title={showStatus === 1 ? '添加分类' : '修改分类'}
+          visible={showStatus !== 0}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+        >
+          {/* 将子组件传递过来的form对象保存到当前组件上 */}
+          <AddUpdateForm setForm={form => this.form = form} categoryName={category.name} />
+        </Modal>
       </Card>
     )
   }
